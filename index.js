329ef0c24cef4f7e9d6cc21748ebe044999ea647
Hatice <haticekaraty@gmail.com>
2021-07-28 20:07:11 -0400

    define setters and getters

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your Circle class here
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+    get diameter(){
+        return this.radius * 2
+    }
+
+    set diameter(newDiameter){
+        this.radius = newDiameter/2
+    } 
+
+    get circumference(){
+        return Math.PI * this.diameter
+    }
+
+    set circumference(newCircumference){
+        this.radius = newCircumference/(2*Math.PI)
+    }
+    get area(){
+        return Math.PI * this.radius ** 2
+    }
+
+    set area(newArea){
+        this.radius = Math.sqrt(newArea/Math.PI);
+    }
+}
+
+// let circle = new Circle(2);
+// console.log(circle.diameter);
+// console.log(circle.circumference);
+// console.log(circle.area);
+// circle.circumference = 20;
+// console.log(circle.area);
+// console.log(circle.radius)
+
